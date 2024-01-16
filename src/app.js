2274dbc7d5820e350f7a148193f714c28933dc3e
anabelena <anabelen.aristah@gmail.com>
2024-01-15 17:10:46 -0800

    agregando componente color y size

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 //Creacion de servidor
 
 import express from "express";
-import { userRouter } from "./components";
+import { userRouter,colorRouter,sizeRouter } from "./components";
 import { apiVersion } from "./config";
 
 export const app = express();
@@ -11,4 +11,5 @@ app.use(express.json());
 
 // app.use(`${apiVersion}/users`, userRouter);
 app.use(`${apiVersion}/stock`, userRouter);
-
+app.use(`${apiVersion}/color`, colorRouter);
+app.use(`${apiVersion}/size`, sizeRouter);
---
+++ src/components/color/controller.js
@@ -0,0 +1,92 @@
+import { prisma } from "../../db";
+import { responseError, responseSuccess } from "../../network/responses";
+
+/* COLOR CRUD */
+
+//READ
+export async function list(req,res) {
+
+    try {
+        const colors = await prisma.color.findMany();
+        return responseSuccess({res,data:colors})
+    }catch(error){
+        return responseError({res,data:error.message})
+    }
+}
+
+
+export async function getById(req,res){
+
+    try {
+        const color = prisma.color.findUnique({
+            where:{
+                id:Number(req.params.id),
+            },
+        });
+
+        if (!color){
+            return responseError({res,data:"Color not found "})
+        }
+        return responseSuccess({res,data:color})
+
+    }catch(error){
+        return responseError({res,data:error.message})
+
+    }
+
+}
+
+//CREATE
+
+export async function store(req,res){
+    try{
+        const color = req.body;
+
+        await prisma.color.create({
+            data:color
+        })
+        return responseSuccess({res,data:"Color created",status:201})
+    }catch(error){
+        return responseError({ res, data: error.message });
+    }
+}
+
+
+//UPDATE
+
+export async function update(req, res) {
+    try {
+      const color = await prisma.color.update({
+        where: {
+          id: Number(req.params.id),
+        },
+        data: req.body,
+      });
+  
+      if (!color) {
+        return responseError({ res, data: "Color not found" });
+      }
+  
+      return responseSuccess({ res, data: "Color updated" });
+    } catch (error) {
+      return responseError({ res, data: error.message });
+    }
+  }
+  
+
+
+//DELETE
+
+export async function destroy(req, res) {
+    try {
+      await prisma.color.delete({
+        where: {
+          id: Number(req.params.id),
+        },
+      });
+      return responseSuccess({ res, data: "Color deleted" });
+    } catch (error) {
+      return responseError({ res, data: error.message });
+    }
+  }
+  
