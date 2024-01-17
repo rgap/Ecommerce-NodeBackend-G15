b29cb274371a895721092aaae0dcdac39d5f76d2
jfalconidc <jorgefalconidc@gmail.com>
2024-01-16 22:32:10 -0500

    Corrección en controller de Productos

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(`${apiVersion}/users`, userRouter);
-app.use(`${apiVersion}/stock`, stockRouter);
 app.use(`${apiVersion}/color`, colorRouter);
 app.use(`${apiVersion}/size`, sizeRouter);
 app.use(`${apiVersion}/product`, productRouter);
