986529ec0b0ee520ccf3af43e3ff3a1762459854
jfalconidc <jorgefalconidc@gmail.com>
2024-01-16 20:00:55 -0500

    Agregando product a index.js y app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 //Creacion de servidor
 
 import express from "express";
-import { userRouter,colorRouter,sizeRouter } from "./components";
+import { userRouter,colorRouter,sizeRouter,productRouter } from "./components";
 import { apiVersion } from "./config";
 
 export const app = express();
@@ -9,6 +9,7 @@ export const app = express();
 app.use(express.json());
 
 app.use(`${apiVersion}/users`, userRouter);
-app.use(`${apiVersion}/stock`, userRouter);
+app.use(`${apiVersion}/stock`, stockRouter);
 app.use(`${apiVersion}/color`, colorRouter);
 app.use(`${apiVersion}/size`, sizeRouter);
+app.use(`${apiVersion}/product`, productRouter);
--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@
 export * from "./user";
 export * from "./color";
 export * from "./size";
+export * from "./product";
 
