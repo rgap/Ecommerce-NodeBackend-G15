b901c340801e5580c7784f62f591302f13c2e474
rgap <r.guzmanap@gmail.com>
2024-01-16 22:11:19 -0500

    data again

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 //Creacion de servidor
 import cors from "cors";
 import express from "express";
-import { userRouter,colorRouter,sizeRouter,productRouter } from "./components";
+import {
+  colorRouter,
+  productRouter,
+  sizeRouter,
+  userRouter,
+} from "./components";
 import { apiVersion } from "./config";
 
 export const app = express();
@@ -10,7 +15,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(`${apiVersion}/users`, userRouter);
-app.use(`${apiVersion}/stock`, stockRouter);
 app.use(`${apiVersion}/color`, colorRouter);
 app.use(`${apiVersion}/size`, sizeRouter);
 app.use(`${apiVersion}/product`, productRouter);
